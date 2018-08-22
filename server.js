da341c8d7013d3f04cab3a379768ef3e41b3269d
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-08-23 09:55:35 +1200

    server using routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const server = express()
 
-server.use('/api/routes', require('./routes/routes'))
+server.get('/', (req, res) => {
+  res.send('hello, world')
+})
 
 module.exports = server
