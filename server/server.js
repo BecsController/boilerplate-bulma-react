f05eae7788645568b909154c3f00fb4274e14358
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-08-23 10:37:50 +1200

    set up basic css file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const server = express()
 
+const path = require('path')
+
 server.use(express.json())
+server.use(express.static(path.join(__dirname, '../public')))
 
 server.use('/api', require('./routes/routes'))
 
